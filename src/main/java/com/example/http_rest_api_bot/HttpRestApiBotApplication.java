505c995ab8b2b2b0d2cdbf696dce60c70781bff3
Gollandra <gollandra761@gmail.com>
2023-09-17 16:06:23 +0300

    initialize project

---
+++ src/main/java/com/example/http_rest_api_bot/HttpRestApiBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.http_rest_api_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpRestApiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpRestApiBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/http_rest_api_bot/HttpRestApiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.http_rest_api_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRestApiBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
